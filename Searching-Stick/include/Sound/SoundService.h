0daf5a6aeeb87c33accabf09348f8776dcf533b1
mridul-outscal <mridul.agrawal@outscal.com>
2024-05-25 00:26:48 +0530

    Reapply "Merge pull request #4 from outscal/Linear-Search-Solution"
    This reverts commit cf02ba98a2b2c23909a4be8075ba51dd52198391.

--- Searching-Stick/include/Sound/SoundService.h
+++ Searching-Stick/include/Sound/SoundService.h
@@ -6,16 +6,19 @@ namespace Sound
 	enum class SoundType
 	{
 		BUTTON_CLICK,
+		COMPARE_SFX,
 	};
 
 	class SoundService
 	{
 	private:
 		const int background_music_volume = 30;
+		const int sound_effect_volume = 200;
 
 		sf::Music background_music;
 		sf::Sound sound_effect;
 		sf::SoundBuffer buffer_button_click;
+		sf::SoundBuffer buffer_compare_sfx;
 
 		void loadBackgroundMusicFromFile();
 		void loadSoundFromFile();
