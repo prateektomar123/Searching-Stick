c50c617d514b6eeac211d92dceea9206c7894e60
mridul-outscal <mridul.agrawal@outscal.com>
2024-05-25 00:31:40 +0530

    Reapply "Merge pull request #5 from outscal/Binary-Search-Solution"
    This reverts commit 6c380dd5c301cdbd7e93eb21ff3364b28f3a6a0a.

--- Searching-Stick/include/Gameplay/StickCollection/StickCollectionController.h
+++ Searching-Stick/include/Gameplay/StickCollection/StickCollectionController.h
@@ -28,7 +28,6 @@ namespace Gameplay {
             int number_of_comparisons;
             int number_of_array_access;
             int current_operation_delay;
-            int num_sticks;
             int delay_in_ms;
             sf::String time_complexity;
 
@@ -38,6 +37,8 @@ namespace Gameplay {
 
             void updateSticksPosition();
             void shuffleSticks();
+            void sortElements();
+            bool compareElementsByData(const Stick* a, const Stick* b) const;
 
             void resetSticksColor();
             void resetVariables();
@@ -46,6 +47,7 @@ namespace Gameplay {
             void processSearchThreadState();
             void joinThreads();
             void processLinearSearch();
+            void processBinarySearch();
             void initializeSticksArray();
             float calculateStickHeight(int array_pos);
 
