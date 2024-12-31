24ccd58bb5d656dc643b045dab15bcd2b886396e
prateektomar123 <prateektomar005@gmail.com>
2024-12-31 19:51:16 +0530

    implemented Gameplay Service

---
+++ Searching-Stick/source/Gameplay/GameplayService.cpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/System/String.hpp>
+
+namespace Gameplay
+{
+	class GameplayController;
+
+	class GameplayService
+	{
+	private:
+		GameplayController* gameplay_controller;
+
+	public:
+		GameplayService();
+		~GameplayService();
+
+		void initialize();
+		void update();
+		void render();
+
+		void reset();
+
+	};
+}
